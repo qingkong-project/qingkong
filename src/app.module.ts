1377f00ba62cbaa2831566cb171765c1aebf316a
zhangtao25 <tzhangm@trip.com>
2022-05-31 22:16:17 +0800

    upd

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,9 +7,18 @@ import { RolesGuard } from './apps/role/guard/roles.guard'
 import { UserModule } from './apps/user/user.module'
 import { AuthModule } from './apps/auth/auth.module'
 import { NoteModule } from './apps/note/note.module'
+import { join } from 'path'
+import { ServeStaticModule } from '@nestjs/serve-static'
 
 @Module({
-  imports: [UserModule, AuthModule, NoteModule],
+  imports: [
+    UserModule,
+    AuthModule,
+    NoteModule,
+    // ServeStaticModule.forRoot({
+    //   rootPath: join(__dirname, '..', 'client'),
+    // }),
+  ],
   controllers: [AppController],
   providers: [
     {
--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,18 @@ import { NestFactory } from '@nestjs/core'
 // import { AppModule } from './app.module'
 import { prepareInit } from './utils/prepareInit'
 import * as bodyParser from 'body-parser'
+import * as express from 'express'
+import * as path from 'path'
 
 async function bootstrap() {
   prepareInit()
   const { AppModule } = await import('./app.module')
   const app = await NestFactory.create(AppModule)
+
+  // 用这种方法可以跳过鉴权
+  //静态资源目录
+  app.use(express.static(path.join(process.cwd(), './client')));
+
   app.use(bodyParser.json({ limit: '50mb' }))
   app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }))
   app.use(function (req, res, next) {
